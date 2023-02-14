bd609f07f5064f1f456151fc2dbd7cc84f82c9d2
Elfilagas <nikita.n.kapustin@gmail.com>
2023-02-14 07:52:03 +0300

    Relations reformat and functions from models to main

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 from dotenv import load_dotenv
 import os
+import json
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from models import create_tables, data_fromJSON_toPSQL, print_sales_by_publisher
+from models import create_tables, Publisher, Book, Shop, Stock, Sale
 
+
+def data_fromJSON_toPSQL(session, file_name: str = 'fixtures/tests_data.json'):
+    """Read data from JSON and write into PostgreSQL DB"""
+    with open(file_name, 'r', encoding='UTF-8') as file:
+        data = json.load(file)
+    for record in data:
+        class_name = {'publisher': Publisher, 
+                      'shop': Shop, 
+                      'book': Book, 
+                      'stock': Stock, 
+                      'sale': Sale
+                      }[record.get('model')]
+        session.add(class_name(id=record.get('pk'), **record.get('fields')))
+    session.commit()
+
+def print_sales_by_publisher(session, pub_name:str):
+    """Print 'book name | shop name | sale price | sale date' for all books of publisher by name or id"""
+    if pub_name.isnumeric():
+        condition = Publisher.id == pub_name
+    else:
+        condition = Publisher.name.like(f'%{pub_name}%')
+
+    q = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
+      join(Publisher).join(Stock).join(Sale).join(Shop).filter(condition).order_by(Sale.date_sale)
+
+    for book, shop, price, count, date in q:
+        print(f'{book:<40} | {shop:<10} | {price*count:<8} | {date}')
 
 def get_DSN_from_dotenv() -> str:
     """Read DSN data from .env file"""
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
-import json
+
 
 Base = declarative_base()
 
@@ -8,18 +8,23 @@ class Publisher(Base):
     __tablename__ = "publisher"
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=100), unique=True, nullable=False)
+    book = relationship("Book")
+
 
 class Book(Base):
     __tablename__ = "book"
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=100), unique=True, nullable=False)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
-    publisher = relationship(Publisher, backref="book_publisher")
+    stock = relationship("Stock")
+
 
 class Shop(Base):
     __tablename__ = "shop"
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=100), unique=True, nullable=False)
+    stock = relationship("Stock")
+
 
 class Stock(Base):
     __tablename__ = "stock"
@@ -27,8 +32,8 @@ class Stock(Base):
     id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
-    book = relationship(Book, backref="stock_book")
-    shop = relationship(Shop, backref="stock_shop")
+    sale = relationship("Sale")
+
 
 class Sale(Base):
     __tablename__ = "sale"
@@ -37,47 +42,10 @@ class Sale(Base):
     date_sale = sq.Column(sq.DateTime, nullable=False)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
-    stock = relationship(Stock, backref="sale_stock")
+    
 
 
 def create_tables(engine):
     """Create all described tables"""
     Base.metadata.drop_all(engine) # for creation test, must be deleted
     Base.metadata.create_all(engine)
-
-def data_fromJSON_toPSQL(session, file_name: str = 'fixtures/tests_data.json'):
-    """Read data from JSON and write into PostgreSQL DB"""
-    with open(file_name, 'r', encoding='UTF-8') as file:
-        data = json.load(file)
-    for record in data:
-        class_name = {'publisher': Publisher, 
-                      'shop': Shop, 
-                      'book': Book, 
-                      'stock': Stock, 
-                      'sale': Sale
-                      }[record.get('model')]
-        session.add(class_name(id=record.get('pk'), **record.get('fields')))
-    session.commit()
-
-def print_sales_by_publisher(session, pub_name:str):
-    """Print 'book name | shop name | sale price | sale date' for all books of publisher by name or id"""
-    if pub_name.isnumeric():
-        condition = Publisher.id == pub_name
-    else:
-        condition = Publisher.name.like(f'%{pub_name}%')
-    q = session.query(Book, Shop, Sale).select_from(Publisher). \
-        join(Book).join(Stock).join(Shop).join(Sale).filter(condition).order_by(Sale.date_sale)
-
-    result = []
-    for book, shop, sale in q:
-        result.append((book.title, shop.name, sale.price, sale.date_sale))
-    if result:
-        for i in result:
-            print(i[0].ljust(max(len(j[0]) for j in result)), 
-                i[1].ljust(max(len(j[1]) for j in result)),
-                str(i[2]).ljust(max(len(str(j[2])) for j in result)), 
-                str(i[3]).ljust(max(len(str(j[3])) for j in result)),  sep=' | ')
-    else:
-        print("По указанному издателю, не найдено записей.")
-
-
